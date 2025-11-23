5dbe43f357e70dc5733c0b6ec234a1c0670397b1
n20va <99407852+n20va@users.noreply.github.com>
2025-11-23 13:50:48 +0300

    removed unused import in UserRepository

--- src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
@@ -8,7 +8,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Repository
 public class UserRepository {
