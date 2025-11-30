40dde6a9394abf6aa9d0f1bcac0ba9125e2db89a
n20va <99407852+n20va@users.noreply.github.com>
2025-11-30 10:57:23 +0300

    Update CommentServiceImpl.java

--- src/main/java/ru/practicum/shareit/item/service/CommentServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/service/CommentServiceImpl.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.booking.repository.BookingRepository;
+import ru.practicum.shareit.booking.Booking;
 import ru.practicum.shareit.exception.NotFoundException;
 import ru.practicum.shareit.exception.ValidationException;
 import ru.practicum.shareit.item.dto.CommentDto;
@@ -77,3 +78,4 @@ public class CommentServiceImpl implements CommentService {
                 .orElseThrow(() -> new NotFoundException("Вещь с ID " + itemId + " не найдена"));
     }
 }
+
