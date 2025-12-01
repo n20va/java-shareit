e6e60596724fa3f31ff6c494cd456389473c4c62
n20va <99407852+n20va@users.noreply.github.com>
2025-12-01 10:02:27 +0300

    Update CommentMapper.java

--- src/main/java/ru/practicum/shareit/item/mapper/CommentMapper.java
+++ src/main/java/ru/practicum/shareit/item/mapper/CommentMapper.java
@@ -1,19 +1,30 @@
 package ru.practicum.shareit.item.mapper;
 
-import org.springframework.stereotype.Component;
 import ru.practicum.shareit.item.dto.CommentDto;
+import ru.practicum.shareit.item.dto.CreateCommentDto;
 import ru.practicum.shareit.item.model.Comment;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.model.User;
 
-@Component
+import java.time.LocalDateTime;
+
 public class CommentMapper {
+    public static Comment toComment(CreateCommentDto createCommentDto, Item item, User author) {
+        Comment comment = new Comment();
+        comment.setText(createCommentDto.getText());
+        comment.setItem(item);
+        comment.setAuthor(author);
+        comment.setCreated(LocalDateTime.now());
+        return comment;
+    }
 
     public static CommentDto toCommentDto(Comment comment) {
-        return new CommentDto(
-                comment.getId(),
-                comment.getText(),
-                comment.getAuthor().getName(),
-                comment.getCreated(),
-                comment.getItem().getId()
-        );
+        CommentDto commentDto = new CommentDto();
+        commentDto.setId(comment.getId());
+        commentDto.setText(comment.getText());
+        commentDto.setAuthorName(comment.getAuthor().getName());
+        commentDto.setCreated(comment.getCreated());
+        commentDto.setItemId(comment.getItem().getId());
+        return commentDto;
     }
 }
