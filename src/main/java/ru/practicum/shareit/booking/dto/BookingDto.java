eb351d3dea7d7ae1e414ee07225aeac722586210
n20va <99407852+n20va@users.noreply.github.com>
2025-12-01 09:36:07 +0300

    Update BookingDto.java

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -2,13 +2,22 @@ package ru.practicum.shareit.booking.dto;
 
 import lombok.Getter;
 import lombok.Setter;
-
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.Future;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 public class BookingDto {
+    @NotNull
     private Long itemId;
+    
+    @NotNull
+    @FutureOrPresent
     private LocalDateTime start;
+    
+    @NotNull
+    @Future
     private LocalDateTime end;
 }
