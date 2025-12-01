3112b1aac693f0a8bdcf8052216c43434fb24bc4
n20va <99407852+n20va@users.noreply.github.com>
2025-12-01 10:09:25 +0300

    Update CommentRepository.java

--- src/main/java/ru/practicum/shareit/item/repository/CommentRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/CommentRepository.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByItemIdOrderByCreatedDesc(Long itemId);
-    
+
     List<Comment> findByItemIdInOrderByCreatedDesc(List<Long> itemIds);
 }
+
