055e34536d6c5464dea80f2ceb9bcd19023ffa38
n20va <99407852+n20va@users.noreply.github.com>
2025-12-07 15:35:33 +0300

    Update UpdateItemDto.java

--- server/src/main/java/ru/practicum/shareit/item/dto/UpdateItemDto.java
+++ server/src/main/java/ru/practicum/shareit/item/dto/UpdateItemDto.java
@@ -1,11 +1,10 @@
 package ru.practicum.shareit.item.dto;
 
 public class UpdateItemDto {
+
     private String name;
     private String description;
     private Boolean available;
-
-    public UpdateItemDto() {}
 
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
@@ -15,4 +14,5 @@ public class UpdateItemDto {
 
     public Boolean getAvailable() { return available; }
     public void setAvailable(Boolean available) { this.available = available; }
+
 }
