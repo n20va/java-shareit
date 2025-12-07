db54c4ad3de2eeed95c3c5ab7af72680597c09bd
n20va <99407852+n20va@users.noreply.github.com>
2025-12-07 14:55:34 +0300

    Refactor User class to remove JPA and Lombok
    Removed JPA annotations and Lombok from User class.

--- server/src/main/java/ru/practicum/shareit/user/model/User.java
+++ server/src/main/java/ru/practicum/shareit/user/model/User.java
@@ -1,53 +1,16 @@
 package ru.practicum.shareit.user.model;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
-import java.util.Objects;
-
-@Entity
-@Table(name = "users")
-@Getter
-@Setter
-@ToString
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(name = "name", nullable = false)
     private String name;
-
-    @Column(name = "email", nullable = false, unique = true, length = 512)
     private String email;
 
-    public User() {
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
-    public User(Long id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        User user = (User) o;
-        return Objects.equals(id, user.id)
-                && Objects.equals(name, user.name)
-                && Objects.equals(email, user.email);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, email);
-    }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 }
