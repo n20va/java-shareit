28dad88cc1269a627a02164eb25664f3975e5b75
n20va <99407852+n20va@users.noreply.github.com>
2025-12-07 14:59:41 +0300

    Refactor CommentMapper methods for parameter order

--- server/src/main/java/ru/practicum/shareit/item/mapper/CommentMapper.java
+++ server/src/main/java/ru/practicum/shareit/item/mapper/CommentMapper.java
@@ -3,28 +3,27 @@ package ru.practicum.shareit.item.mapper;
 import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.dto.CreateCommentDto;
 import ru.practicum.shareit.item.model.Comment;
-import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
-
+import ru.practicum.shareit.item.model.Item;
 import java.time.LocalDateTime;
 
 public class CommentMapper {
-    public static Comment toComment(CreateCommentDto createCommentDto, Item item, User author) {
+
+    public static Comment toComment(CreateCommentDto dto, User author, Item item) {
         Comment comment = new Comment();
-        comment.setText(createCommentDto.getText());
-        comment.setItem(item);
+        comment.setText(dto.getText());
         comment.setAuthor(author);
+        comment.setItem(item);
         comment.setCreated(LocalDateTime.now());
         return comment;
     }
 
     public static CommentDto toCommentDto(Comment comment) {
-        CommentDto commentDto = new CommentDto();
-        commentDto.setId(comment.getId());
-        commentDto.setText(comment.getText());
-        commentDto.setAuthorName(comment.getAuthor().getName());
-        commentDto.setCreated(comment.getCreated());
-        commentDto.setItemId(comment.getItem().getId());
-        return commentDto;
+        CommentDto dto = new CommentDto();
+        dto.setId(comment.getId());
+        dto.setText(comment.getText());
+        dto.setAuthorName(comment.getAuthor().getName());
+        dto.setCreated(comment.getCreated());
+        return dto;
     }
 }
