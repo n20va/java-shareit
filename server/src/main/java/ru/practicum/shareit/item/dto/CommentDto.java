e7104bde2bcf49316b700fe6117407e9556c6bc1
n20va <99407852+n20va@users.noreply.github.com>
2025-12-07 15:09:35 +0300

    Refactor CommentDto to remove Lombok annotations

--- server/src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
+++ server/src/main/java/ru/practicum/shareit/item/dto/CommentDto.java
@@ -1,20 +1,24 @@
 package ru.practicum.shareit.item.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class CommentDto {
     private Long id;
     private String text;
     private String authorName;
     private LocalDateTime created;
-    private Long itemId;
+
+    public CommentDto() {}
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getText() { return text; }
+    public void setText(String text) { this.text = text; }
+
+    public String getAuthorName() { return authorName; }
+    public void setAuthorName(String authorName) { this.authorName = authorName; }
+
+    public LocalDateTime getCreated() { return created; }
+    public void setCreated(LocalDateTime created) { this.created = created; }
 }
