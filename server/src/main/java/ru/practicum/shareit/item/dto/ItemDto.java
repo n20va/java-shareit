51292916a64c005ab70cec50f6a4e4d30118b823
n20va <99407852+n20va@users.noreply.github.com>
2025-12-07 15:34:49 +0300

    Update ItemDto.java

--- server/src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ server/src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -3,16 +3,13 @@ package ru.practicum.shareit.item.dto;
 import java.util.List;
 
 public class ItemDto {
+
     private Long id;
     private String name;
     private String description;
     private Boolean available;
     private Long requestId;
-    private BookingInfo lastBooking;
-    private BookingInfo nextBooking;
     private List<CommentDto> comments;
-
-    public ItemDto() {}
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
@@ -29,29 +26,7 @@ public class ItemDto {
     public Long getRequestId() { return requestId; }
     public void setRequestId(Long requestId) { this.requestId = requestId; }
 
-    public BookingInfo getLastBooking() { return lastBooking; }
-    public void setLastBooking(BookingInfo lastBooking) { this.lastBooking = lastBooking; }
-
-    public BookingInfo getNextBooking() { return nextBooking; }
-    public void setNextBooking(BookingInfo nextBooking) { this.nextBooking = nextBooking; }
-
     public List<CommentDto> getComments() { return comments; }
     public void setComments(List<CommentDto> comments) { this.comments = comments; }
 
-    public static class BookingInfo {
-        private Long id;
-        private Long bookerId;
-
-        public BookingInfo() {}
-        public BookingInfo(Long id, Long bookerId) {
-            this.id = id;
-            this.bookerId = bookerId;
-        }
-
-        public Long getId() { return id; }
-        public void setId(Long id) { this.id = id; }
-
-        public Long getBookerId() { return bookerId; }
-        public void setBookerId(Long bookerId) { this.bookerId = bookerId; }
-    }
 }
