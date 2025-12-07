af9e87dafd03fd8510b4da3705aeedf42626151f
n20va <99407852+n20va@users.noreply.github.com>
2025-12-07 15:24:55 +0300

    Update Item.java

--- server/src/main/java/ru/practicum/shareit/item/model/Item.java
+++ server/src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.item.model;
 
 import ru.practicum.shareit.user.model.User;
-import ru.practicum.shareit.request.model.ItemRequest;
 
 public class Item {
     private Long id;
@@ -9,9 +8,9 @@ public class Item {
     private String description;
     private Boolean available;
     private User owner;
-    private ItemRequest request;
 
     public Item() {}
+
     public Item(Long id, String name, String description, Boolean available, User owner) {
         this.id = id;
         this.name = name;
@@ -20,23 +19,43 @@ public class Item {
         this.owner = owner;
     }
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public Long getId() {
+        return id;
+    }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
+    public String getName() {
+        return name;
+    }
 
-    public Boolean getAvailable() { return available; }
-    public void setAvailable(Boolean available) { this.available = available; }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    public User getOwner() { return owner; }
-    public void setOwner(User owner) { this.owner = owner; }
+    public String getDescription() {
+        return description;
+    }
 
-    public Long getOwnerId() { return owner != null ? owner.getId() : null; }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-    public ItemRequest getRequest() { return request; }
-    public void setRequest(ItemRequest request) { this.request = request; }
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
 }
