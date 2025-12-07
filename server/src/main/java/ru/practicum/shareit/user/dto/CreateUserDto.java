8d8a58e9dc439b744cafd42308c6d5bacd619746
n20va <99407852+n20va@users.noreply.github.com>
2025-12-07 14:51:24 +0300

    Refactor CreateUserDto to use explicit methods
    Replaced Lombok annotations with explicit getter and setter methods.

--- server/src/main/java/ru/practicum/shareit/user/dto/CreateUserDto.java
+++ server/src/main/java/ru/practicum/shareit/user/dto/CreateUserDto.java
@@ -1,11 +1,12 @@
 package ru.practicum.shareit.user.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class CreateUserDto {
     private String name;
     private String email;
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 }
