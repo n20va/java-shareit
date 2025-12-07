d1fdc20b10af13e71cc65e4e1a0b627cd1ac6b15
n20va <99407852+n20va@users.noreply.github.com>
2025-12-07 14:54:11 +0300

    Refactor BookingResponseDto to use UserDto and String status

--- server/src/main/java/ru/practicum/shareit/booking/dto/BookingResponseDto.java
+++ server/src/main/java/ru/practicum/shareit/booking/dto/BookingResponseDto.java
@@ -1,35 +1,43 @@
 package ru.practicum.shareit.booking.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-import ru.practicum.shareit.booking.status.BookingStatus;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.user.dto.UserDto;
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
-@ToString
 public class BookingResponseDto {
     private Long id;
     private LocalDateTime start;
     private LocalDateTime end;
-    private BookingStatus status;
+    private String status;
     private BookerDto booker;
     private ItemDto item;
 
-    @Getter
-    @Setter
-    @ToString
     public static class BookerDto {
         private Long id;
         private String name;
+
+        public Long getId() { return id; }
+        public void setId(Long id) { this.id = id; }
+
+        public String getName() { return name; }
+        public void setName(String name) { this.name = name; }
     }
 
-    @Getter
-    @Setter
-    @ToString
-    public static class ItemDto {
-        private Long id;
-        private String name;
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public LocalDateTime getStart() { return start; }
+    public void setStart(LocalDateTime start) { this.start = start; }
+
+    public LocalDateTime getEnd() { return end; }
+    public void setEnd(LocalDateTime end) { this.end = end; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+
+    public BookerDto getBooker() { return booker; }
+    public void setBooker(BookerDto booker) { this.booker = booker; }
+
+    public ItemDto getItem() { return item; }
+    public void setItem(ItemDto item) { this.item = item; }
 }
