2fd806136a261e46a09630b5a9b9ecc309891579
n20va <99407852+n20va@users.noreply.github.com>
2025-12-07 14:04:21 +0300

    Update ItemRequestMapper.java

--- src/main/java/ru/practicum/shareit/request/mapper/ItemRequestMapper.java
+++ src/main/java/ru/practicum/shareit/request/mapper/ItemRequestMapper.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.request.mapper;
 
 import org.springframework.stereotype.Component;
 import ru.practicum.shareit.item.dto.ItemDto;
-import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.item.service.ItemService;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
 import ru.practicum.shareit.request.model.ItemRequest;
