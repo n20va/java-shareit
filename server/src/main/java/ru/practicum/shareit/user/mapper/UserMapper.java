355e9c6cbf7377171fb4bdef8fef886f92b85784
n20va <99407852+n20va@users.noreply.github.com>
2025-12-07 15:15:22 +0300

    Update UserMapper.java

--- server/src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
+++ server/src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
@@ -1,13 +1,13 @@
 package ru.practicum.shareit.user.mapper;
 
+import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.dto.CreateUserDto;
 import ru.practicum.shareit.user.dto.UpdateUserDto;
 import ru.practicum.shareit.user.dto.UserDto;
-import ru.practicum.shareit.user.model.User;
 
 public class UserMapper {
 
-    public static User toUser(CreateUserDto dto) {
+    public static User toUserFromCreateDto(CreateUserDto dto) {
         if (dto == null) return null;
         User user = new User();
         user.setName(dto.getName());
@@ -15,11 +15,10 @@ public class UserMapper {
         return user;
     }
 
-    public static User toUser(UpdateUserDto dto, User existingUser) {
-        if (dto == null) return null;
-        if (dto.getName() != null) existingUser.setName(dto.getName());
-        if (dto.getEmail() != null) existingUser.setEmail(dto.getEmail());
-        return existingUser;
+    public static void updateUserFromDto(UpdateUserDto dto, User user) {
+        if (dto == null || user == null) return;
+        if (dto.getName() != null) user.setName(dto.getName());
+        if (dto.getEmail() != null) user.setEmail(dto.getEmail());
     }
 
     public static UserDto toUserDto(User user) {
