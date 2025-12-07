f4439e569b5517df784ec6140496ef0171895622
n20va <99407852+n20va@users.noreply.github.com>
2025-12-07 14:53:39 +0300

    Refactor BookingDto to use manual getters and setters
    Replaced Lombok annotations with manual getter and setter methods for BookingDto fields.

--- server/src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ server/src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -1,13 +1,18 @@
 package ru.practicum.shareit.booking.dto;
 
-import lombok.Getter;
-import lombok.Setter;
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
 public class BookingDto {
     private Long itemId;
     private LocalDateTime start;
     private LocalDateTime end;
+
+    public Long getItemId() { return itemId; }
+    public void setItemId(Long itemId) { this.itemId = itemId; }
+
+    public LocalDateTime getStart() { return start; }
+    public void setStart(LocalDateTime start) { this.start = start; }
+
+    public LocalDateTime getEnd() { return end; }
+    public void setEnd(LocalDateTime end) { this.end = end; }
 }
