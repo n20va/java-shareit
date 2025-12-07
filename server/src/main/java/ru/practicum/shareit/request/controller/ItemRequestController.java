90f5141771d78399c1fe98e68eaa22937b1d35cb
n20va <99407852+n20va@users.noreply.github.com>
2025-12-07 15:00:32 +0300

    Refactor ItemRequestController and add ItemRequestMapper

--- server/src/main/java/ru/practicum/shareit/request/controller/ItemRequestController.java
+++ server/src/main/java/ru/practicum/shareit/request/controller/ItemRequestController.java
@@ -1,51 +1,28 @@
-package ru.practicum.shareit.request.controller;
+package ru.practicum.shareit.request.mapper;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.request.dto.CreateItemRequestDto;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
-import ru.practicum.shareit.request.service.ItemRequestService;
-
+import ru.practicum.shareit.request.model.ItemRequest;
+import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.item.dto.ItemDto;
 import java.util.List;
 
-@RestController
-@RequestMapping(path = "/requests")
-@RequiredArgsConstructor
-public class ItemRequestController {
-    private final ItemRequestService requestService;
+public class ItemRequestMapper {
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public ItemRequestDto createRequest(
-            @RequestBody CreateItemRequestDto createItemRequestDto,
-            @RequestHeader("X-Sharer-User-Id") Long requesterId
-    ) {
-        return requestService.createRequest(createItemRequestDto, requesterId);
+    public static ItemRequest toItemRequest(CreateItemRequestDto dto, User requester) {
+        ItemRequest request = new ItemRequest();
+        request.setDescription(dto.getDescription());
+        request.setRequester(requester);
+        request.setCreated(java.time.LocalDateTime.now());
+        return request;
     }
 
-    @GetMapping
-    public List<ItemRequestDto> getUserRequests(
-            @RequestHeader("X-Sharer-User-Id") Long requesterId
-    ) {
-        return requestService.getUserRequests(requesterId);
+    public static ItemRequestDto toItemRequestDto(ItemRequest request, List<ItemDto> items) {
+        ItemRequestDto dto = new ItemRequestDto();
+        dto.setId(request.getId());
+        dto.setDescription(request.getDescription());
+        dto.setCreated(request.getCreated());
+        dto.setItems(items);
+        return dto;
     }
-
-    @GetMapping("/all")
-    public List<ItemRequestDto> getAllRequests(
-            @RequestHeader("X-Sharer-User-Id") Long userId,
-            @RequestParam(defaultValue = "0") Integer from,
-            @RequestParam(defaultValue = "10") Integer size
-    ) {
-        return requestService.getAllRequests(userId, from, size);
-    }
-
-    @GetMapping("/{requestId}")
-    public ItemRequestDto getRequestById(
-            @PathVariable Long requestId,
-            @RequestHeader("X-Sharer-User-Id") Long userId
-    ) {
-        return requestService.getRequestById(requestId, userId);
-    }
-
 }
