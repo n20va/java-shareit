2cc0763036f40ef1b913672375fb2c60976786f1
n20va <99407852+n20va@users.noreply.github.com>
2025-12-07 14:56:30 +0300

    Refactor Comment class by removing JPA annotations

--- server/src/main/java/ru/practicum/shareit/item/model/Comment.java
+++ server/src/main/java/ru/practicum/shareit/item/model/Comment.java
@@ -1,67 +1,27 @@
 package ru.practicum.shareit.item.model;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
 import ru.practicum.shareit.user.model.User;
-
 import java.time.LocalDateTime;
-import java.util.Objects;
 
-@Entity
-@Table(name = "comments")
-@Getter
-@Setter
-@ToString
 public class Comment {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(name = "text", nullable = false, length = 512)
     private String text;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "item_id", nullable = false)
     private Item item;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "author_id", nullable = false)
     private User author;
-
-    @Column(name = "created", nullable = false)
     private LocalDateTime created;
 
-    public Comment() {
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
-    public Comment(Long id, String text, Item item, User author, LocalDateTime created) {
-        this.id = id;
-        this.text = text;
-        this.item = item;
-        this.author = author;
-        this.created = created;
-    }
+    public String getText() { return text; }
+    public void setText(String text) { this.text = text; }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Comment comment = (Comment) o;
-        return Objects.equals(id, comment.id)
-                && Objects.equals(text, comment.text)
-                && Objects.equals(item, comment.item)
-                && Objects.equals(author, comment.author)
-                && Objects.equals(created, comment.created);
-    }
+    public Item getItem() { return item; }
+    public void setItem(Item item) { this.item = item; }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, text, item, author, created);
-    }
+    public User getAuthor() { return author; }
+    public void setAuthor(User author) { this.author = author; }
+
+    public LocalDateTime getCreated() { return created; }
+    public void setCreated(LocalDateTime created) { this.created = created; }
 }
