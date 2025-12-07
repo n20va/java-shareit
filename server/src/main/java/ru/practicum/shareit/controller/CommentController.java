a4a19210956e4eb65423be0810f3a0b1b085856e
n20va <99407852+n20va@users.noreply.github.com>
2025-12-07 21:45:16 +0300

    Update CommentController.java

--- server/src/main/java/ru/practicum/shareit/controller/CommentController.java
+++ server/src/main/java/ru/practicum/shareit/controller/CommentController.java
@@ -4,12 +4,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.dto.comment.CommentDto;
 import ru.practicum.shareit.service.CommentService;
+
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/items")
 public class CommentController {
+
+    private final CommentService commentService;
 
     @PostMapping("/{itemId}/comment")
     public CommentDto addComment(
