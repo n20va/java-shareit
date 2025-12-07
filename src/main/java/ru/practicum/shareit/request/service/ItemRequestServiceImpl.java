81d16577a80f3d13e0a6309735728dfb88b4524d
n20va <99407852+n20va@users.noreply.github.com>
2025-12-07 14:04:44 +0300

    Update ItemRequestServiceImpl.java

--- src/main/java/ru/practicum/shareit/request/service/ItemRequestServiceImpl.java
+++ src/main/java/ru/practicum/shareit/request/service/ItemRequestServiceImpl.java
@@ -6,7 +6,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.exception.NotFoundException;
-import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.item.repository.ItemRepository;
 import ru.practicum.shareit.request.dto.CreateItemRequestDto;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
