01a0de2db4210c4aad3c68a253373fb7de1f4821
n20va <99407852+n20va@users.noreply.github.com>
2025-12-07 21:41:56 +0300

    Update CommentController.java

--- server/src/main/java/ru/practicum/shareit/controller/CommentController.java
+++ server/src/main/java/ru/practicum/shareit/controller/CommentController.java
@@ -11,20 +11,16 @@ import java.util.List;
 @RequestMapping("/items")
 public class CommentController {
 
-    private final CommentService commentService;
-
-    @PostMapping("/items/{itemId}/comment")
-public CommentDto addComment(
-        @RequestHeader("X-Sharer-User-Id") Long userId,
-        @PathVariable Long itemId,
-        @RequestBody CommentDto dto) {
-    return commentService.addComment(userId, itemId, dto);
-}
-
+    @PostMapping("/{itemId}/comment")
+    public CommentDto addComment(
+            @RequestHeader("X-Sharer-User-Id") Long userId,
+            @PathVariable Long itemId,
+            @RequestBody CommentDto dto) {
+        return commentService.addComment(userId, itemId, dto);
+    }
 
     @GetMapping("/{itemId}/comments")
-    public List<CommentDto> getCommentsByItem(
-            @PathVariable Long itemId) {
+    public List<CommentDto> getCommentsByItem(@PathVariable Long itemId) {
         return commentService.getCommentsByItem(itemId);
     }
 }
