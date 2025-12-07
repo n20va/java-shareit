4d04260bda95bcc00168df4fe9a2c1323bd979c4
n20va <99407852+n20va@users.noreply.github.com>
2025-12-07 14:50:50 +0300

    Refactor UpdateItemDto to use explicit methods
    Replaced Lombok annotations with explicit getter and setter methods.

--- server/src/main/java/ru/practicum/shareit/item/dto/UpdateItemDto.java
+++ server/src/main/java/ru/practicum/shareit/item/dto/UpdateItemDto.java
@@ -1,12 +1,16 @@
 package ru.practicum.shareit.item.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class UpdateItemDto {
     private String name;
     private String description;
     private Boolean available;
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public Boolean getAvailable() { return available; }
+    public void setAvailable(Boolean available) { this.available = available; }
 }
