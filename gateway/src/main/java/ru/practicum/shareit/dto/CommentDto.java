30faee38985468e7d132ab5ff34499fa8ddde09d
n20va <99407852+n20va@users.noreply.github.com>
2025-12-07 22:14:20 +0300

    Update CommentDto.java

--- gateway/src/main/java/ru/practicum/shareit/dto/CommentDto.java
+++ gateway/src/main/java/ru/practicum/shareit/dto/CommentDto.java
@@ -8,15 +8,14 @@ import java.time.LocalDateTime;
 
 @Data
 public class CommentDto {
+
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long id;
 
     @NotBlank(message = "Текст комментария не может быть пустым")
     private String text;
 
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private String authorName;
 
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private LocalDateTime created;
 }
