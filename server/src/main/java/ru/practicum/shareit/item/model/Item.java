880687843c453179bc9a476c812c2156a26f1953
n20va <99407852+n20va@users.noreply.github.com>
2025-12-07 15:46:04 +0300

    Update Item.java

--- server/src/main/java/ru/practicum/shareit/item/model/Item.java
+++ server/src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -14,6 +14,15 @@ public class Item {
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public Boolean getAvailable() { return available; }
+    public void setAvailable(Boolean available) { this.available = available; }
+
     public User getOwner() { return owner; }
     public void setOwner(User owner) { this.owner = owner; }
     public Long getOwnerId() { return owner != null ? owner.getId() : null; }
