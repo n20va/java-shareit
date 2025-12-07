39f6888aecbd09cdf76f727347ccef7b3678ffdd
n20va <99407852+n20va@users.noreply.github.com>
2025-12-07 15:27:50 +0300

    Update Item.java

--- server/src/main/java/ru/practicum/shareit/item/model/Item.java
+++ server/src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.item.model;
 
 import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.request.model.ItemRequest;
 
 public class Item {
     private Long id;
@@ -8,54 +9,42 @@ public class Item {
     private String description;
     private Boolean available;
     private User owner;
+    private ItemRequest request;
 
     public Item() {}
 
-    public Item(Long id, String name, String description, Boolean available, User owner) {
+    public Item(Long id, String name, String description, Boolean available, User owner, ItemRequest request) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.available = available;
         this.owner = owner;
+        this.request = request;
     }
 
-    public Long getId() {
-        return id;
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public Boolean getAvailable() { return available; }
+    public void setAvailable(Boolean available) { this.available = available; }
+
+    public User getOwner() { return owner; }
+    public void setOwner(User owner) { this.owner = owner; }
+
+    public ItemRequest getRequest() { return request; }
+    public void setRequest(ItemRequest request) { this.request = request; }
+
+    public Long getOwnerId() {
+        return owner != null ? owner.getId() : null;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Boolean getAvailable() {
-        return available;
-    }
-
-    public void setAvailable(Boolean available) {
-        this.available = available;
-    }
-
-    public User getOwner() {
-        return owner;
-    }
-
-    public void setOwner(User owner) {
-        this.owner = owner;
+    public Long getRequestId() {
+        return request != null ? request.getId() : null;
     }
 }
