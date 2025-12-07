57b3a50ead2808b23610c43543d13015f35a72c0
n20va <99407852+n20va@users.noreply.github.com>
2025-12-07 14:22:44 +0300

    Refactor ItemMapper to use Lombok's UtilityClass
    Refactor ItemMapper to use Lombok's UtilityClass for cleaner code and simplify methods.

--- server/src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
+++ server/src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -1,67 +1,67 @@
 package ru.practicum.shareit.item.mapper;
 
-import org.springframework.stereotype.Component;
-import ru.practicum.shareit.item.dto.CommentDto;
-import ru.practicum.shareit.item.dto.CreateItemDto;
-import ru.practicum.shareit.item.dto.ItemDto;
-import ru.practicum.shareit.item.dto.UpdateItemDto;
+import lombok.experimental.UtilityClass;
+import ru.practicum.shareit.item.dto.*;
 import ru.practicum.shareit.item.model.Item;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-@Component
+@UtilityClass
 public class ItemMapper {
 
+    public Item toItemFromCreateDto(CreateItemDto dto, Long ownerId) {
+        return new Item(
+                null,
+                dto.getName(),
+                dto.getDescription(),
+                dto.getAvailable(),
+                ownerId,
+                dto.getRequestId()
+        );
+    }
+
+    public void updateItemFromDto(UpdateItemDto dto, Item item) {
+        if (dto.getName() != null) item.setName(dto.getName());
+        if (dto.getDescription() != null) item.setDescription(dto.getDescription());
+        if (dto.getAvailable() != null) item.setAvailable(dto.getAvailable());
+    }
+
     public ItemDto toItemDto(Item item) {
-        return toItemDto(item, null, null, Collections.emptyList());
+        return new ItemDto(
+                item.getId(),
+                item.getName(),
+                item.getDescription(),
+                item.getAvailable(),
+                null,
+                null,
+                List.of()
+        );
     }
 
-    public ItemDto toItemDto(Item item, ItemDto.BookingInfo lastBooking,
-                             ItemDto.BookingInfo nextBooking, List<CommentDto> comments) {
-        ItemDto itemDto = new ItemDto();
-        itemDto.setId(item.getId());
-        itemDto.setName(item.getName());
-        itemDto.setDescription(item.getDescription());
-        itemDto.setAvailable(item.getAvailable());
-        itemDto.setRequestId(item.getRequestId());
-        itemDto.setLastBooking(lastBooking);
-        itemDto.setNextBooking(nextBooking);
-        itemDto.setComments(comments != null ? comments : new ArrayList<>());
-        return itemDto;
+    public ItemDto toItemDto(Item item,
+                             ItemDto.BookingInfo lastBooking,
+                             ItemDto.BookingInfo nextBooking,
+                             List<CommentDto> comments) {
+        return new ItemDto(
+                item.getId(),
+                item.getName(),
+                item.getDescription(),
+                item.getAvailable(),
+                lastBooking,
+                nextBooking,
+                comments
+        );
     }
 
-    public Item toItemFromCreateDto(CreateItemDto createItemDto, Long ownerId) {
-        if (createItemDto == null) {
-            return null;
-        }
-        Item item = new Item();
-        item.setName(createItemDto.getName());
-        item.setDescription(createItemDto.getDescription());
-        item.setAvailable(createItemDto.getAvailable());
-        item.setOwnerId(ownerId);
-        item.setRequestId(createItemDto.getRequestId());
-        return item;
-    }
-
-    public Item updateItemFromDto(UpdateItemDto updateItemDto, Item item) {
-        if (updateItemDto == null || item == null) {
-            return item;
-        }
-
-        if (updateItemDto.getName() != null && !updateItemDto.getName().isBlank()) {
-            item.setName(updateItemDto.getName());
-        }
-
-        if (updateItemDto.getDescription() != null && !updateItemDto.getDescription().isBlank()) {
-            item.setDescription(updateItemDto.getDescription());
-        }
-
-        if (updateItemDto.getAvailable() != null) {
-            item.setAvailable(updateItemDto.getAvailable());
-        }
-
-        return item;
+    public ItemDto toItemDtoWithComments(Item item, List<CommentDto> comments) {
+        return new ItemDto(
+                item.getId(),
+                item.getName(),
+                item.getDescription(),
+                item.getAvailable(),
+                null,
+                null,
+                comments
+        );
     }
 }
