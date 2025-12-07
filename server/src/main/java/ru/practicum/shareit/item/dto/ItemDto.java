4c66abf3f4bdc92a896f2830b88cdccd74f136e7
n20va <99407852+n20va@users.noreply.github.com>
2025-12-07 14:52:55 +0300

    Refactor ItemDto by simplifying structure
    Removed unnecessary annotations and nested BookingInfo class.

--- server/src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ server/src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,100 +1,24 @@
 package ru.practicum.shareit.item.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Objects;
-
-@Getter
-@Setter
-@ToString
 public class ItemDto {
     private Long id;
     private String name;
     private String description;
     private Boolean available;
     private Long requestId;
-    private BookingInfo lastBooking;
-    private BookingInfo nextBooking;
-    private List<CommentDto> comments;
 
-    @Getter
-    @Setter
-    @ToString
-    public static class BookingInfo {
-        private Long id;
-        private Long bookerId;
-        private LocalDateTime start;
-        private LocalDateTime end;
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
-        public BookingInfo() {
-        }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
-        public BookingInfo(Long id, Long bookerId) {
-            this.id = id;
-            this.bookerId = bookerId;
-        }
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
 
-        public BookingInfo(Long id, Long bookerId, LocalDateTime start, LocalDateTime end) {
-            this.id = id;
-            this.bookerId = bookerId;
-            this.start = start;
-            this.end = end;
-        }
+    public Boolean getAvailable() { return available; }
+    public void setAvailable(Boolean available) { this.available = available; }
 
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            BookingInfo that = (BookingInfo) o;
-            return Objects.equals(id, that.id) &&
-                    Objects.equals(bookerId, that.bookerId) &&
-                    Objects.equals(start, that.start) &&
-                    Objects.equals(end, that.end);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(id, bookerId, start, end);
-        }
-    }
-
-    public ItemDto() {
-    }
-
-    public ItemDto(Long id, String name, String description, Boolean available, Long requestId) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.available = available;
-        this.requestId = requestId;
-    }
-
-    public ItemDto(Long id, String name, String description, Boolean available) {
-        this(id, name, description, available, null);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        ItemDto itemDto = (ItemDto) o;
-        return Objects.equals(id, itemDto.id)
-                && Objects.equals(name, itemDto.name)
-                && Objects.equals(description, itemDto.description)
-                && Objects.equals(available, itemDto.available)
-                && Objects.equals(requestId, itemDto.requestId);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, description, available, requestId);
-    }
+    public Long getRequestId() { return requestId; }
+    public void setRequestId(Long requestId) { this.requestId = requestId; }
 }
