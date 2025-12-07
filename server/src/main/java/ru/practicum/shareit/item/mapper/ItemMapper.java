ccb0d07e8a9f9d656dab0bb0e293fa956189f24b
n20va <99407852+n20va@users.noreply.github.com>
2025-12-07 15:36:24 +0300

    Update ItemMapper.java

--- server/src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
+++ server/src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -3,35 +3,34 @@ package ru.practicum.shareit.item.mapper;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.dto.CommentDto;
-import ru.practicum.shareit.item.dto.CreateItemDto;
-import ru.practicum.shareit.item.dto.UpdateItemDto;
 
 import java.util.List;
 
 public class ItemMapper {
 
-    public static ItemDto toItemDto(Item item, List<CommentDto> comments) {
+    public ItemDto toItemDto(Item item, List<CommentDto> comments) {
         if (item == null) return null;
         ItemDto dto = new ItemDto();
         dto.setId(item.getId());
         dto.setName(item.getName());
         dto.setDescription(item.getDescription());
         dto.setAvailable(item.getAvailable());
-        dto.setRequestId(item.getRequest() != null ? item.getRequest().getId() : null);
+        dto.setRequestId(item.getRequestId());
         dto.setComments(comments);
         return dto;
     }
 
-    public static Item toItemFromCreateDto(CreateItemDto dto, Long ownerId) {
+    public Item toItem(CreateItemDto dto) {
         if (dto == null) return null;
         Item item = new Item();
         item.setName(dto.getName());
         item.setDescription(dto.getDescription());
         item.setAvailable(dto.getAvailable());
+        item.setRequestId(dto.getRequestId());
         return item;
     }
 
-    public static void updateItemFromDto(UpdateItemDto dto, Item item) {
+    public void updateItemFromDto(UpdateItemDto dto, Item item) {
         if (dto.getName() != null) item.setName(dto.getName());
         if (dto.getDescription() != null) item.setDescription(dto.getDescription());
         if (dto.getAvailable() != null) item.setAvailable(dto.getAvailable());
