2ca8fb6ec298f9ee253615b0018d7d3cb2ba1e88
n20va <99407852+n20va@users.noreply.github.com>
2025-12-07 14:50:15 +0300

    Update CreateItemDto.java

--- server/src/main/java/ru/practicum/shareit/item/dto/CreateItemDto.java
+++ server/src/main/java/ru/practicum/shareit/item/dto/CreateItemDto.java
@@ -1,13 +1,20 @@
 package ru.practicum.shareit.item.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public class CreateItemDto {
     private String name;
     private String description;
     private Boolean available;
     private Long requestId;
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public Boolean getAvailable() { return available; }
+    public void setAvailable(Boolean available) { this.available = available; }
+
+    public Long getRequestId() { return requestId; }
+    public void setRequestId(Long requestId) { this.requestId = requestId; }
 }
