92fc4c8e4877338608f4527e3932a8948d2798d2
n20va <99407852+n20va@users.noreply.github.com>
2025-12-08 22:24:29 +0300

    Update CommentRepository.java

--- server/src/main/java/ru/practicum/shareit/repository/CommentRepository.java
+++ server/src/main/java/ru/practicum/shareit/repository/CommentRepository.java
@@ -3,10 +3,11 @@ package ru.practicum.shareit.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.shareit.entity.Comment;
 
-
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     List<Comment> findAllByItemId(Long itemId);
+
+    List<Comment> findAllByItemIdIn(List<Long> itemIds);
 }
